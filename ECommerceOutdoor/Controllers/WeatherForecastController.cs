a77803741f059b5a0cc0eccaa66828c31302676f
shalvagagnidze <salvagagnidze@gmail.com>
2024-07-16 17:36:32 +0400

    Initial Commit

--- ECommerceOutdoor/Controllers/WeatherForecastController.cs
+++ ECommerceOutdoor/Controllers/WeatherForecastController.cs
@@ -6,10 +6,6 @@ namespace ECommerceOutdoor.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
