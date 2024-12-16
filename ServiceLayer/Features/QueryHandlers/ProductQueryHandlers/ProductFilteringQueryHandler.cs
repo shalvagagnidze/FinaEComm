35a66515bf9c93e634cf9b2fcace58d61a979137
Natia Changhlishvili <natia.changhlishvili@blocshop.io>
2024-12-16 23:02:27 +0400

    fixed

--- ServiceLayer/Features/QueryHandlers/ProductQueryHandlers/ProductFilteringQueryHandler.cs
+++ ServiceLayer/Features/QueryHandlers/ProductQueryHandlers/ProductFilteringQueryHandler.cs
@@ -53,7 +53,7 @@ public class ProductFilteringQueryHandler : IRequestHandler<ProductFilteringQuer
 
         foreach(var ff in request.filter.FacetFilters!)
         {
-            products = products.Where(x => !x.ProductFacetValues.IsNullOrEmpty() &&  x.ProductFacetValues.Any(f => f.FacetValueId == ff.facetValueId));
+            products = products.Where(x => x.ProductFacetValues != null && x.ProductFacetValues.Count > 0 &&  x.ProductFacetValues.Any(f => f.FacetValueId == ff.facetValueId));
         }
 
         var productModelsQuery = products.Select(b => new ProductModel
