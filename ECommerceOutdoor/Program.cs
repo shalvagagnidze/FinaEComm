483382a1f646279d06cb04de60b6499d51d4b83e
shalvagagnidze <salvagagnidze@gmail.com>
2025-01-07 17:06:07 +0400

    Added .gitignore

--- ECommerceOutdoor/Program.cs
+++ ECommerceOutdoor/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddCors(options =>
         });
 });
 
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
